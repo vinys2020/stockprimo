08577353b7c595578cf4403a02d370f52a8e1804
vinys2020 <noraelizabethibarra@macbookair.home>
2025-08-02 13:15:28 -0300

    home

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenido a StockPrimo</h1>
+      <p>Esta es la página principal de tu app de manejo de stock.</p>
+    </div>
+  );
+}
+
